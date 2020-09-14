18e74f2882bf82ceebd24c96423351cf85d7d6ef
Tsairenkun <Tsairenkun@gmail.com>
2020-09-14 14:32:41 +0800

    sudo code and the steps to complete this question

---
+++ sum_zero.js
@@ -0,0 +1,8 @@
+function sum_zero(arr){
+    //split array into 2
+    let left = 0;
+    //counting from the left 
+    //counting from the right
+    //compare each value from the left and right to make it 0
+    //return
+}
