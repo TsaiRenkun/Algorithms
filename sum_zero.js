245ae9e3f545073fc9fcd7c9bf84ace5acab311b
Tsairenkun <Tsairenkun@gmail.com>
2020-09-14 14:38:44 +0800

    Solution

--- sum_zero.js
+++ sum_zero.js
@@ -1,8 +1,20 @@
 function sum_zero(arr){
     //split array into 2
     let left = 0;
+    let right = arr.length - 1
     //counting from the left 
     //counting from the right
-    //compare each value from the left and right to make it 0
-    //return
+    while(left < right){
+        let sum = arr[left] + arr[right]
+        if(sum === 0 ){
+            return [[arr[left],arr[right]]]
+        }
+        else if (sum > 0){
+            right--;
+        } else {
+            left++;
+        }
+    }
 }
+
+console.log(sum_zero([-4,-3,-2,0,1,3,5]))
