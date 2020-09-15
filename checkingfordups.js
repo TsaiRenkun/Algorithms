9f89a8491f6435e9028bc4a8f6d67b0e15f2eeb2
Tsairenkun <Tsairenkun@gmail.com>
2020-09-15 15:31:08 +0800

    Checking for dups, practice question

---
+++ checkingfordups.js
@@ -0,0 +1,5 @@
+function areThereDuplicates() {
+    //take input and stringfy
+    //create object to and check if values are repeated
+    // good luck. (supply any arguments you deem necessary.)
+  }
