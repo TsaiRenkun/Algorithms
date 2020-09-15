ee79415a42342de9ac52fcc6bff68b8285c1a2cb
Tsairenkun <Tsairenkun@gmail.com>
2020-09-15 15:48:30 +0800

    average Pair

---
+++ multipointavepair.js
@@ -0,0 +1,5 @@
+function averagePair(arr, num){
+    //check if arr length != 0
+    //break array up starting from front and back
+    // add whatever parameters you deem necessary - good luck!
+  }
