bed3b282b19e0a1615ef106c5ff51b84ed97d9c2
Tsairenkun <Tsairenkun@gmail.com>
2020-09-15 15:43:44 +0800

    Completed solution for checking dups

--- checkingfordups.js
+++ checkingfordups.js
@@ -1,5 +1,16 @@
 function areThereDuplicates() {
-    //take input and stringfy
+    let collection = {}
+
+    for(let val in arguments){
+        collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for(let key in collection){
+        if(collection[key] > 1) return true;
+    }
+    return false
     //create object to and check if values are repeated
     // good luck. (supply any arguments you deem necessary.)
   }
+
+console.log(areThereDuplicates("a","b","c","c"))
+// areThereDuplicates(1,2,2)
