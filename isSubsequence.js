040964f975bb4af3855556a4a347735cba6bccb9
Tsairenkun <Tsairenkun@gmail.com>
2020-09-21 13:15:42 +0800

    Multipointers Exercise 6

---
+++ isSubsequence.js
@@ -0,0 +1,6 @@
+function isSubsequence(word1,word2) {
+    //Take in 2 String
+    //Create 2 objs to hold both strings
+    //Each having a key and index
+    // good luck. Add any arguments you deem necessary.
+  }
