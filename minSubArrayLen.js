032a40c30b9d2408d4ae7de79ee14e939cd85663
Tsairenkun <Tsairenkun@gmail.com>
2020-09-22 23:52:27 +0800

    solution for min subArrayLen

--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -1,3 +1,27 @@
 function minSubArrayLen(arr,num){
+    let start = 0;
+    let end = 0;
+    let total = 0;
+    let minLen = Infinity;
+
+    while(start < arr.length){
+
+        if(total < num && end < arr.length){
+            total += arr[end];
+            end ++
+        } 
+        else if (total >= sum){
+            minLen = Math.min(minLen, end-start);
+            total -= nums[start]
+            start ++
+        } else {
+            break;
+        }
+        return minLen === Infinity ? 0 : minLen 
+    }
+    
+    
     
 }
+
+console.log(minSubArrayLen([2,3,1,2,4,3],7))
