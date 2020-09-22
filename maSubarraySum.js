42a2f2b33f311be565c06a9d56fd9edd9d013d26
Tsairenkun <Tsairenkun@gmail.com>
2020-09-22 23:18:33 +0800

    max sum of a sub array

---
+++ maSubarraySum.js
@@ -0,0 +1,9 @@
+function maxSubarraySum(){
+    
+    // add whatever parameters you deem necessary - good luck!
+  }
+
+maxSubarraySum([100,200,300,400],2)
+maxSubarraySum([1,4,2,10,23,3,1,0,20],4)
+maxSubarraySum([-3,4,0,-2,6,-1],2)
+maxSubarraySum([2,3],3)
