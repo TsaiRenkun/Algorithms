8e58b304c239eb9d039338b132743add2571738c
Tsairenkun <Tsairenkun@gmail.com>
2020-09-22 23:22:46 +0800

    Sudo code for solution

--- maSubarraySum.js
+++ maSubarraySum.js
@@ -1,7 +1,11 @@
-function maxSubarraySum(){
-    
+function maxSubarraySum(arr, num){
+    //make sure num is smaller then arr.length
+    //finding the subarray
+    //total the array 
+    //total the sub array
     // add whatever parameters you deem necessary - good luck!
   }
+
 
 maxSubarraySum([100,200,300,400],2)
 maxSubarraySum([1,4,2,10,23,3,1,0,20],4)
